484f3dc84a1931e7ead01c8f14e4e889a0011259
hagano <hagano@duck.cm>
2023-04-14 00:41:10 -0300

    Init launcher

--- app/src/main/java/com/example/dev_mobile/MainActivity.java
+++ app/src/main/java/com/example/dev_mobile/MainActivity.java
@@ -4,45 +4,30 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Toast;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
-    RecyclerView recyclerView;
-    FrutaController frutaController;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        recyclerView = findViewById(R.id.recyclerView);
-        recyclerView.setHasFixedSize(true);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
-        frutaController = new FrutaController();
-        FrutaAdapter frutaAdapter = new FrutaAdapter(this, R.layout.item_lista, frutaController.FRUTAS);
+        PackageManager pm = getPackageManager();
+        List<ApplicationInfo> listApps;
+        listApps = pm.getInstalledApplications(PackageManager.GET_META_DATA);
 
-        recyclerView.setAdapter(frutaAdapter);
-
-        recyclerView.addOnItemTouchListener(new RecyclerItemClickListener(getApplicationContext(),
-                recyclerView,
-                new RecyclerItemClickListener.OnItemClickListener() {
-                    @Override
-                    public void onItemClick(View view, int position) {
-                        Toast.makeText(getApplicationContext(), frutaAdapter.mDataSet[position].getNome(), Toast.LENGTH_LONG).show();
-                    }
-
-                    @Override
-                    public void onLongItemClick(View view, int position) {
-
-                    }
-
-                    @Override
-                    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-
-                    }
-                }
-        ));
+        for (ApplicationInfo appinfo:listApps) {
+            Log.d("App", appinfo.packageName + pm.getApplicationLabel(appinfo).toString());
+            // Se desejado, poderia printar o ícone setando como drawable.
+//            imageView.setDrawable(pm.getApplicationIcon(appinfo));
+        }
     }
 }
