678545e946e7c11cb05a816e13f0bdd007e86f3b
hagano <hagano@duck.cm>
2023-04-14 00:19:11 -0300

    Implement onClickListener

--- app/src/main/java/com/example/dev_mobile/MainActivity.java
+++ app/src/main/java/com/example/dev_mobile/MainActivity.java
@@ -5,6 +5,9 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     RecyclerView recyclerView;
@@ -21,5 +24,25 @@ public class MainActivity extends AppCompatActivity {
         FrutaAdapter frutaAdapter = new FrutaAdapter(this, R.layout.item_lista, frutaController.FRUTAS);
 
         recyclerView.setAdapter(frutaAdapter);
+
+        recyclerView.addOnItemTouchListener(new RecyclerItemClickListener(getApplicationContext(),
+                recyclerView,
+                new RecyclerItemClickListener.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(View view, int position) {
+                        Toast.makeText(getApplicationContext(), frutaAdapter.mDataSet[position].getNome(), Toast.LENGTH_LONG).show();
+                    }
+
+                    @Override
+                    public void onLongItemClick(View view, int position) {
+
+                    }
+
+                    @Override
+                    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                    }
+                }
+        ));
     }
 }
