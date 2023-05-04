6fa86925469332d8b9cdfbbcfee8991e03c56a9f
haganothecat <hagano@duck.com>
2023-05-04 20:05:08 -0300

    MediaPlayer proto

--- app/src/main/java/com/example/dev_mobile/MainActivity.java
+++ app/src/main/java/com/example/dev_mobile/MainActivity.java
@@ -2,13 +2,49 @@ package com.example.dev_mobile;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    MediaPlayer mediaPlayer;
+    ImageButton btnPlay, btnStop, btnPause;
+    TextView textViewTime;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mediaPlayer = MediaPlayer.create(this, R.raw.seminovos);
+
+        btnStop = findViewById(R.id.btnStop);
+        btnPlay = findViewById(R.id.btnPlay);
+        btnPause = findViewById(R.id.btnPause);
+        textViewTime = findViewById(R.id.textViewTime);
+
+        btnStop.setOnClickListener(this);
+        btnPlay.setOnClickListener(this);
+        btnPause.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        switch (view.getId()) {
+            case R.id.btnPlay:
+                mediaPlayer.start();
+                while (mediaPlayer.isPlaying()) {
+                    textViewTime.setText(mediaPlayer.getCurrentPosition());
+                }
+                break;
+            case R.id.btnStop:
+                mediaPlayer.stop();
+                break;
+            case R.id.btnPause:
+                mediaPlayer.pause();
+                break;
+        }
     }
 }
