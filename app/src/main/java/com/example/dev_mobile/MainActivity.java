79bf7a29dd01edbffea391d29019647923292791
haganothecat <114202875+haganothecat@users.noreply.github.com>
2023-05-17 22:21:35 -0300

    Handler atualiza tempo de playback

--- app/src/main/java/com/example/dev_mobile/MainActivity.java
+++ app/src/main/java/com/example/dev_mobile/MainActivity.java
@@ -2,16 +2,23 @@ package com.example.dev_mobile;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
+
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     MediaPlayer mediaPlayer;
     ImageButton btnPlay, btnStop, btnPause;
     TextView textViewTime;
+    Handler handler;
+    boolean isPlaying = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,24 +34,62 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btnPlay.setOnClickListener(this);
         btnPause.setOnClickListener(this);
 
+        handler = new Handler();
     }
 
+    @SuppressLint("NonConstantResourceId")
     @Override
     public void onClick(View view) {
-
         switch (view.getId()) {
             case R.id.btnPlay:
-                mediaPlayer.start();
-                while (mediaPlayer.isPlaying()) {
-                    textViewTime.setText(mediaPlayer.getCurrentPosition());
+                if (!isPlaying) {
+                    mediaPlayer.start();
+                    isPlaying = true;
+                    updateTextViewTime();
                 }
                 break;
             case R.id.btnStop:
-                mediaPlayer.stop();
+                if (isPlaying) {
+                    mediaPlayer.stop();
+                    mediaPlayer.reset();
+                    isPlaying = false;
+                    textViewTime.setText("00:00");
+                    handler.removeCallbacksAndMessages(null);
+                }
                 break;
             case R.id.btnPause:
-                mediaPlayer.pause();
+                if (isPlaying) {
+                    mediaPlayer.pause();
+                    isPlaying = false;
+                    handler.removeCallbacksAndMessages(null);
+                }
                 break;
         }
+    }
+
+    private void updateTextViewTime() {
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if (mediaPlayer.isPlaying()) {
+                    int currentPosition = mediaPlayer.getCurrentPosition();
+                    textViewTime.setText(convertDurationMillis(currentPosition));
+                    updateTextViewTime();
+                }
+            }
+        }, 1000);
+    }
+    public String convertDurationMillis(Integer getDurationInMillis){
+
+        int getDurationMillis = getDurationInMillis;
+        String convertHours = String.format("%02d", TimeUnit.MILLISECONDS.toHours(getDurationMillis));
+        String convertMinutes = String.format("%02d", TimeUnit.MILLISECONDS.toMinutes(getDurationMillis));
+        String convertSeconds = String.format("%02d", TimeUnit.MILLISECONDS.toSeconds(getDurationMillis) -
+                TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(getDurationMillis)));
+
+//        String getDuration = convertHours + ":" + convertMinutes + ":" + convertSeconds;
+        String getDuration = convertMinutes + ":" + convertSeconds;
+
+        return getDuration;
     }
 }
