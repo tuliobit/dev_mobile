7b5e361083ce231df51d26d1fe986f8ceaf59690
haganothecat <114202875+haganothecat@users.noreply.github.com>
2023-05-21 23:06:55 -0300

    initial commit

--- app/src/main/java/com/example/dev_mobile/MainActivity.java
+++ app/src/main/java/com/example/dev_mobile/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.dev_mobile;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SQLiteDatabase db = openOrCreateDatabase("dbTaco", Context.MODE_PRIVATE, null);
+
+        String sql = AssetUtils.loadStringFromAsset(Context, "taco_4_edicao.sql");
+        String[] statements = sql.split(";");
+        for (String statement : statements) {
+            db.execSQL(statement);
+        }
     }
 }
